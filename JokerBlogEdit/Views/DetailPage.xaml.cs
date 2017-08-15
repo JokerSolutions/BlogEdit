0545f96dd1a2edf93728a1a3ca3241bfcb8b87de
Sebastian Meier zu Biesen <smzb@mitos-kalandiel.me>
2017-08-15 23:33:11 +0100

    added project to master
    at this point in time we have only just added the new VS2017 Project with a Template10 project template, based on Win10SDK_10.0.14393.0 (as this is a base requirement for any Template10apps, it appears)

---
+++ JokerBlogEdit/Views/DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using JokerBlogEdit.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace JokerBlogEdit.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
