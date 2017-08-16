f4255ed1927697ed8c1c66187efb2cc23e3b46ba
Sebastian Meier zu Biesen <smzb@mitos-kalandiel.me>
2017-08-16 09:55:08 +0100

    removed faulty template project i've now added a functioning wpf app with the basic functions of any app (such as a menu etc)

--- BlogEdit/Properties/AssemblyInfo.cs
+++ BlogEdit/Properties/AssemblyInfo.cs
@@ -5,14 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JokerBlogEdit")]
+[assembly: AssemblyTitle("BlogEdit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JokerBlogEdit")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("BlogEdit")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f6a150fa-ed81-4290-bb13-eafff2ea669f")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -24,6 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.1.*")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// App.config was added to enable Log4Net logger
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
